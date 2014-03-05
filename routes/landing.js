3d799183510d6a5da5565c671c136da1d9de3539
Nathan Eidelson <nathanje@stanford.edu>
2014-03-05 15:49:17 -0800

    wayy improved friend selection

--- routes/landing.js
+++ routes/landing.js
@@ -7,25 +7,23 @@ var request = require('request');
 var models = require('../models');
 
 exports.view = function(req, res) {
-  console.log("1");
+
   if (req.query.alternate === 'true') {
     req.session.alternate = true;
   } else if (req.query.alternate === 'false') {
     req.session.alternate = false;
   }
   
-  if (NODE_ENV == 'localhost') {
-      console.log("2");
-    req.session.fb_access_token = "CAAIoq33YZCcIBAAuo65r70EmXofY7ZALAaDWrjXHXcnrInZB3AVWNZBfhsSpoKJFQXOMBZBK0iJMUvVCClv5b7UxNp4SAbe6CiJOko1rvQm4tcB9LA7kbpqki5De0aTh3ZAzpPF1I2KezOX2FY1oC5qdZCjG0WGAC9gJ7CNui3dPPPtgGhAzf80uvw2EuM40rQZD";
-    req.session.user_id = 1495998285;
-  }
-
-  console.log("3");
+  // For local dev (uncomment this, and comment out all GoogleAnalytics scripts): nathan
+  // if (NODE_ENV == 'localhost') {
+  //     console.log("2");
+  //   req.session.fb_access_token = "CAAIoq33YZCcIBAAuo65r70EmXofY7ZALAaDWrjXHXcnrInZB3AVWNZBfhsSpoKJFQXOMBZBK0iJMUvVCClv5b7UxNp4SAbe6CiJOko1rvQm4tcB9LA7kbpqki5De0aTh3ZAzpPF1I2KezOX2FY1oC5qdZCjG0WGAC9gJ7CNui3dPPPtgGhAzf80uvw2EuM40rQZD";
+  //   req.session.user_id = 1495998285;
+  // }
 
   if (req.session.fb_access_token) {
     res.redirect("feed");
   } else {
-      console.log("4");
 
     if (NODE_ENV == 'localhost') {
       var redirect_url = encodeURIComponent("http://localhost:3000/fblogin");
